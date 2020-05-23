35197ee9c2cab9e31ef977441e28d57709115a54
hellowluan <hellowluan@gmail.com>
2020-05-22 22:13:39 -0300

    login working

--- login.php
+++ login.php
@@ -1,5 +1,25 @@
 <?php
   session_start();
+  require("config.php");
+
+  if(isset($_SESSION['id']) == 0) {
+    if(isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['password']) && !empty($_POST['password'])) {
+      $user = addslashes($_POST['username']);
+      $password = md5(addslashes($_POST['password']));
+
+      $sql = "SELECT * FROM login_sys WHERE user = '$user' AND pass = '$password'";
+      $sql = $pdo->query($sql);
+
+      if($sql->rowCount() > 0) {
+        $data = $sql->fetch();
+        $_SESSION['id'] = $data['id'];
+        header("Location: index.php");
+      }
+    }
+  } else {
+    header("Location: index.php");
+  }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,9 +37,9 @@
     <img src="src/img/do-utilizador.svg" class="user-icon">
     <form method="POST" class="form">
       <label for="username">Username</label>
-      <input type="text">
+      <input type="text" name="username">
       <label for="password">Password</label>
-      <input type="password">
+      <input type="password" name="password">
 
       <input type="submit" value="Login">
 
